35683d761d3c8d04859447b76a280f6960b46bf7
sanqianjie <1342350720@qq.com>
2016-06-20 17:43:21 +0800

    DB添加

---
+++ app/src/main/java/com/mirror/mirrortry/db/DBValues.java
@@ -0,0 +1,16 @@
+package com.mirror.mirrortry.db;
+
+/**
+ * Created by dllo on 16/6/20.
+ * db用初始值
+ */
+public class DBValues {
+    //库名,表名
+    public static final String DB_NAME = "mirror.db";
+    public static final String TABLE_NAME = "mirror";
+
+    //key值 及 内容名字
+    public static final String COLUMN_DATA_ID = "data_id";
+    public static final String COLUMN_DATA_CONTEXT = "data_context";
+
+}

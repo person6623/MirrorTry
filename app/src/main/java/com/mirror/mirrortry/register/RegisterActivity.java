86f59630c66d7ddeeb4df9e3b842591029c210de
HanYang111 <280952129@qq.com>
2016-06-21 16:49:09 +0800

    创建登录和注册界面的布局

---
+++ app/src/main/java/com/mirror/mirrortry/register/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.mirror.mirrortry.register;
+
+import com.mirror.mirrortry.R;
+import com.mirror.mirrortry.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/6/21.
+ */
+public class RegisterActivity extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}

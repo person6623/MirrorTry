9c90142cec6bed30c3d0fe87a57b61ab5901f1ae
HanYang111 <280952129@qq.com>
2016-06-21 20:46:49 +0800

    no message

--- app/src/main/java/com/mirror/mirrortry/main/MainActivity.java
+++ app/src/main/java/com/mirror/mirrortry/main/MainActivity.java
@@ -1,27 +1,32 @@
 package com.mirror.mirrortry.main;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.mirror.mirrortry.R;
+import com.mirror.mirrortry.login.LoginActivity;
 import com.mirror.mirrortry.verticalviewpager.VerticalViewPager;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private VerticalViewPager viewPager;
-    private ArrayList<Fragment>fragments;
+    private ArrayList<Fragment> fragments;
     private MainViewPagerAdapter adapter;
-    private int position;
+    private TextView login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (VerticalViewPager) findViewById(R.id.main_viewPager);
+        login = (TextView) findViewById(R.id.login);
+        login.setOnClickListener(this);
+
         adapter = new MainViewPagerAdapter(getSupportFragmentManager());
         Fragment all = AllKindFragment.createFragment();
         Fragment goggles = AllKindFragment.createFragment();
@@ -35,5 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+        }
+    }
 }

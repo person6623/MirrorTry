b4d511c547b03b963cd696c577fde2660f6b7dce
sanqianjie <1342350720@qq.com>
2016-06-27 09:03:25 +0800

    未更改

--- app/src/main/java/com/mirror/mirrortry/glassdetails/GlassDetailsActivity.java
+++ app/src/main/java/com/mirror/mirrortry/glassdetails/GlassDetailsActivity.java
@@ -5,10 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-<<<<<<< HEAD
-=======
+
 import android.view.LayoutInflater;
->>>>>>> feature/眼镜详情
+
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
